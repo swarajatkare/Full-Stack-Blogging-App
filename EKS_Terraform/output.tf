5060245e1617e91048921e58af323821665c5926
Swaraj_Atkare <161420580+swarajatkare@users.noreply.github.com>
2025-02-07 16:46:31 +0530

    Update output.tf

--- EKS_Terraform/output.tf
+++ EKS_Terraform/output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.swarajatkare.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.swarajatkare.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.swarajatkare_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.swarajatkare_subnet[*].id
 }
